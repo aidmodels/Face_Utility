e3531c17beb9456b3efd20c9a638e1cd9d835afd
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-09-11 12:16:24 +0800

    reset pretrained file for vcs

--- face_utility/bundle.py
+++ face_utility/bundle.py
@@ -2,7 +2,7 @@ from pkg_resources import resource_filename
 from cvpm.bundle import Bundle
 
 class FaceUtilityBundle(Bundle):
-    PRETRAINED_TOML = resource_filename(__name__, "../pretrained/pretrained.toml")
+    PRETRAINED_TOML = resource_filename(__name__, "../pretrained.toml")
     POSE_PREDICTOR_LOCATION = resource_filename(__name__, "../pretrained/shape_predictor_68_face_landmarks.dat")
     CNN_FACE_DETECTOR_LOCATION = resource_filename(__name__, "../pretrained/mmod_human_face_detector.dat")
     POSE_PREDICTOR_FIVE_LOCATION = resource_filename(__name__, "../pretrained/shape_predictor_5_face_landmarks.dat")
