21d578b5b8d54cba597010787776d925738790b5
407718364 <407718364@qq.com>
2018-09-22 23:34:53 -0400

    add landmark feature

--- face_utility/solver.py
+++ face_utility/solver.py
@@ -37,7 +37,7 @@ class FaceLandmarkSolver(Solver):
         self.logger = logging.getLogger('face_utility')
         self.pose_estimator_68 = dlib.shape_predictor(Bundle.POSE_PREDICTOR_LOCATION)
         self.pose_estimator_5 = dlib.shape_predictor(Bundle.POSE_PREDICTOR_FIVE_LOCATION)
-        self.fds = FaceDetectionSolver()
+        self.face_detector = dlib.get_frontal_face_detector()
         self.detection_config = {
             "mode": "HOG",
             "number_of_times_to_upsample": "1"
@@ -45,6 +45,37 @@ class FaceLandmarkSolver(Solver):
         self.set_bundle(Bundle)
         self._enable_train = Bundle.ENABLE_TRAIN
         self.set_ready()
+        
     def infer(self, image_file, config):
+        landmarks = []
+        pose_predictor = None
         image_np = load_image_file(image_file)
+        face_locations = self.face_detector(image_np, int(self.detection_config["number_of_times_to_upsample"]))
+        if config["mode"] == "small":
+            pose_predictor = self.pose_estimator_5
+        else:
+            pose_predictor = self.pose_estimator_68
+        landmarks = [pose_predictor(image_np, face_location) for face_location in face_locations]
+        landmarks_as_tuples = [[(p.x, p.y) for p in landmark.parts()] for landmark in landmarks]
+        if config["mode"] == "large":
+            return [{
+                "chin": points[0:17],
+                "left_eyebrow": points[17:22],
+                "right_eyebrow": points[22:27],
+                "nose_bridge": points[27:31],
+                "nose_tip": points[31:36],
+                "left_eye": points[36:42],
+                "right_eye": points[42:48],
+                "top_lip": points[48:55] + [points[64]] + [points[63]] + [points[62]] + [points[61]] + [points[60]],
+                "bottom_lip": points[54:60] + [points[48]] + [points[60]] + [points[67]] + [points[66]] + [points[65]] + [points[64]]
+            } for points in landmarks_as_tuples]
+        elif config["mode"] == "small":
+            return [{
+                "nose_tip": [points[4]],
+                "left_eye": points[2:4],
+                "right_eye": points[0:2],
+            } for points in landmarks_as_tuples]
+        else:
+            self.logger.error("[FACE UTILITY] Only small and large are supported mode yet!")            
+        
         
